dc16a3fddf15280e82675536dee7de218be52561
KillickNZ <sebastian.g.chapman@gmail.com>
2021-03-26 11:11:23 +1300

    Quick tidy up

--- read.js
+++ read.js
@@ -4,15 +4,12 @@ const fs = require("fs");
 function read (filepath, res, req, callback) {
     fs.readFile(filepath, (err, data) => { 
         if (err) callback(err);
-
         else callback(err, res, req, data)
-
     })
 }
 
 function renderHome(err, res, req, data) {
     if (err) console.log(err)
-
     const viewData = {
         pageClass: " home",
         // puppies: JSON.parse(data),
@@ -29,10 +26,6 @@ function renderStory(err, res, req, data) {
     if (err) console.log(err)
     else {res.render('story', data)}
 }
-
-
-
-
 
 module.exports = {
     read,
