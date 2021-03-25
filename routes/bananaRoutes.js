5bafb148237bb3a416681dbd632170cbd0a1f376
student <student@eda-bc-01.localdomain>
2021-03-26 11:38:26 +1300

    blank

--- routes/bananaRoutes.js
+++ routes/bananaRoutes.js
@@ -19,22 +19,22 @@ module.exports = router;
 //MADLIIB ROUTES
 //ROUTES
 
-server.get("/compliment", (request, response) => {
-    response.send("<h1>You are amazing!!</h1>");
-});
+// server.get("/compliment", (request, response) => {
+//     response.send("<h1>You are amazing!!</h1>");
+// });
 
-server.get("/profile", (request, response) => {
-    const name = request.query.name;
-    response.sendFile(__dirname + "/" + name + ".html");
-});
+// server.get("/profile", (request, response) => {
+//     const name = request.query.name;
+//     response.sendFile(__dirname + "/" + name + ".html");
+// });
 
-server.get("/profiles/:id", (request, response) => {
-    const name = { 1: "silvia", 2: "sampson" };
-    const id = request.params.id;
-    response.sendFile(__dirname + "/" + name[id] + ".html");
-});
+// server.get("/profiles/:id", (request, response) => {
+//     const name = { 1: "silvia", 2: "sampson" };
+//     const id = request.params.id;
+//     response.sendFile(__dirname + "/" + name[id] + ".html");
+// });
 
-server.post('/madlib', (req, res) => {
-    console.log(req.body)
-    res.send("<h1>I love " + req.body.noun + " and I am a " + req.body.adjective + " person</h1>")
-}) 
+// server.post('/madlib', (req, res) => {
+//     console.log(req.body)
+//     res.send("<h1>I love " + req.body.noun + " and I am a " + req.body.adjective + " person</h1>")
+// }) 
