5c36c2d0965a22e69604790227925ad504c5db8c
KillickNZ <sebastian.g.chapman@gmail.com>
2021-03-26 14:23:25 +1300

    Edited form data

--- read.js
+++ read.js
@@ -33,13 +33,14 @@ function renderStory(err, res, req, data) {
         obj.adjective4 = req.body.adjective4
         obj.adjective5 = req.body.adjective5
         obj.adjective6 = req.body.adjective6
+        obj.noun7 = req.body.noun7
         obj.time_in_the_past8 = req.body.time_in_the_past8
         obj.noun9 = req.body.noun9
         obj.noun10 = req.body.noun10
         obj.noun11 = req.body.noun11
         obj.past_tense_verb13 = req.body.past_tense_verb13
         obj.adjective14 = req.body.adjective14
-        obj.ver15 = req.body.verb15
+        obj.verb15 = req.body.verb15
 
         const newData = JSON.stringify(obj)
 
